9cba3cdceaed098407a6d0abd47b7205e4bc7ab2
Makka Akhrieva <makka.akhrieva00@mail.ru>
2022-01-11 21:42:11 +0300

    create template for employers app

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import './search-panel.css'
+
+const SearchPanel=()=>{
+    return(
+        <input  type="text"
+                className="form-control seach-input"
+                placeholder="Найти сотрудника"/>
+    )
+}
+
+export default SearchPanel;
