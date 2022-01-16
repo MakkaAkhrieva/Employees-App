d03933ceed9427bd5fcd31890473c01dfdd6b721
Makka Akhrieva <makka.akhrieva00@mail.ru>
2022-01-16 22:03:58 +0300

    add/delete employers functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,33 +1,76 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
-import './app.css';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 
+import './app.css';
 
 
+class App extends Component{
 
-function App(){
+    constructor(props){
+        super(props);
+        this.state={
+            data:[
+               {name:'Makka',salary:500,increase:true,id:1},
+               {name:'Madina',salary:3000 ,increase:false,id:2},
+               {name:'Alina',salary:800,increase:false,id:3} 
+            ]
+        }
+        this.maxId=4;
+    }
 
-    const data=[
-        {name:'Makka',salary:500,increase:true,id:1},
-        {name:'Madina',salary:3000 ,increase:false,id:2},
-        {name:'Alina',salary:800,increase:false,id:3}
-    ]
-
-    return(
-        <div className="app">
-            <AppInfo/>
+    deleteItem =(id)=>{
+        this.setState(({data})=>{
+          /*   const index=data.findIndex(elem=>elem.id==id); */
             
-            <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+          /*   const before=data.slice(0,index);
+            const after=data.slice(index+1);
+            const newArr=[...before, ...after]; */
+
+
+
+            return{
+                data:data.filter(item=>item.id!==id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+
+    render(){
+        return(
+            <div className="app">
+                <AppInfo/>
+                
+                <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+                </div>
+                <EmployersList
+                 data={this.state.data}
+                 onDelete={this.deleteItem}/>
+                <EmployersAddForm onAddItem={this.addItem}/>
             </div>
-            <EmployersList data={data}/>
-            <EmployersAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -20,6 +20,15 @@ class EmployersAddForm extends Component{
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAddItem(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
  render(){
 
     const{name,salary}=this.state
@@ -28,7 +37,8 @@ class EmployersAddForm extends Component{
         <div className="app-add-form">
             <h3>Добавьте нового сотрудника</h3>
             <form
-                className="add-form d-flex">
+                className="add-form d-flex"
+                onSubmit={this.onSubmit}>
                 <input type="text"
                     className="form-control new-post-label"
                     placeholder="Как его зовут?"
